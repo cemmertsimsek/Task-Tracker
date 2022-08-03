49665d101c5d17651258919559007cfdbee25a2a
cem <cemsimseksm@gmail.com>
2022-08-03 16:38:15 +0300

    react files and components created

---
+++ solution/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import AddTaskForm from "./AddTaskForm";
+
+
+const Header = () => {
+  return (
+  <header>
+    <h1>TASK TRACKER</h1>
+    <AddTaskForm />
+  </header>
+  );
+};
+
+
+export default Header;

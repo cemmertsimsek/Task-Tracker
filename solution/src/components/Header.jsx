7b919640f94ae47474f3bbab5ef428e23907e58e
cem <cemsimseksm@gmail.com>
2022-08-03 17:05:25 +0300

    open close task bar button updated

--- solution/src/components/Header.jsx
+++ solution/src/components/Header.jsx
@@ -1,11 +1,37 @@
 import AddTaskForm from "./AddTaskForm";
-
+import { useState } from "react";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({
+    name: 'SHOW ADDTASK BAR',
+    bgColor: 'purple',
+  });
+  const handleShow = () => {
+    if(show){
+      setBtnStyle({
+        name: 'SHOW ADDTASK BAR',
+        bgColor: 'purple',
+      })
+    }else{
+      setBtnStyle({
+        name: 'CLOSE ADDTASK BAR',
+        bgColor: 'red',
+      })
+    }
+    setShow(!show)
+  };
+
   return (
-  <header>
+  <header className="header">
     <h1>TASK TRACKER</h1>
-    <AddTaskForm />
+    <button 
+      onClick={handleShow} 
+      className="btn" 
+      style={{backgroundColor: btnStyle.bgColor}}>
+      {btnStyle.name}
+    </button>
+    {show && <AddTaskForm />}
   </header>
   );
 };
